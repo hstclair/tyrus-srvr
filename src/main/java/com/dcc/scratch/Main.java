489374ac88c6c41f995f5a907ab63e7ddcd8197e
Hal St. Clair <hal.stclair@airbus-dscomm.com>
2018-01-26 15:04:45 -0600

    initial

---
+++ src/main/java/com/dcc/scratch/Main.java
@@ -0,0 +1,10 @@
+package com.dcc.scratch;
+
+public class Main {
+
+    static public void main(String[] args) {
+        TestServer testServer = new TestServer();
+
+        testServer.runServer();
+    }
+}
---
+++ src/main/java/com/dcc/scratch/TestServer.java
@@ -0,0 +1,30 @@
+package com.dcc.scratch;
+
+import org.glassfish.tyrus.server.Server;
+
+import javax.websocket.DeploymentException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TestServer {
+
+    public void runServer() {
+
+        Server server = new Server("localhost", 8025, "/websockets", null, EchoEndpoint.class);
+
+        try {
+            server.start();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("Please press <Enter> to stop the server.");
+            reader.readLine();
+        } catch (IOException | DeploymentException ex) {
+            ex.printStackTrace();
+        } finally {
+            server.stop();
+        }
+    }
+
+}
